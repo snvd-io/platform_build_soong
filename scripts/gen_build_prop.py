8692cc10398424e4a78e80b568bad0cc71b37e8e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-06 14:29:02 +0200

    enable ro.control_privapp_permissions=enforce
    Original change:
    https://github.com/GrapheneOS/platform_build/commit/1b81d3a891ef113fc8874e1732827cc4b222f5cb

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -363,6 +363,8 @@ def append_additional_system_props(args):
   # This property is set by flashing debug boot image, so default to false.
   props.append("ro.force.debuggable=0")
 
+  props.append("ro.control_privapp_permissions=enforce")
+
   config["ADDITIONAL_SYSTEM_PROPERTIES"] = props
 
 def append_additional_vendor_props(args):
