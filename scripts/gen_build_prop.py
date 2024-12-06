875efb8c8f32c73788d3a62bbb74c181a4cc6849
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-06 14:32:08 +0200

    reduce auto time update threshold

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -365,6 +365,9 @@ def append_additional_system_props(args):
 
   props.append("ro.control_privapp_permissions=enforce")
   props.append("net.tethering.noprovisioning=true")
+  # System clock update will be skipped if the difference is below this value,
+  # default is 2000 ms as of Android 14.
+  props.append("ro.sys.time_detector_update_diff=50")
 
   config["ADDITIONAL_SYSTEM_PROPERTIES"] = props
 
